e64157fc80271b9dd9129705bcaddfd2863c9459
Larry glebov <larry.glebov@gmail.com>
2022-03-10 13:03:44 +0100

    update styles

--- src/components/FormGenerator/styles.ts
+++ src/components/FormGenerator/styles.ts
@@ -1,10 +1,8 @@
 import styled from '@emotion/styled'
 
 export const FormGeneratorContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
   width: 100%;
+  min-height: calc(100vh - 30px * 6);
   margin: 30px auto;
   padding: 10px;
   border-radius: 10px;
